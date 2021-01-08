bf5e0b5c960e68c62f45bb535bbab877907922f3
stephenfranz22@gmail.com <stephenfranz22@gmail.com>
2021-01-08 15:10:56 -0500

    add json storage, error handling

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
+
+# ---------------------------- SEARCH ------------------------------- #
+
+def search():
+    search_string = website_text.get("1.0", END).strip()
+    try:
+        with open("data.json", mode="r") as data_file:
+            # reading old data
+            data = json.load(data_file)
+            print(data)
+    except FileNotFoundError:
+        messagebox.showerror(title="No Data", message="Database is Empty")
+        password_text.delete("1.0", END)
+        email_text.delete("1.0", END)
+        website_text.delete("1.0", END)
+
+    else:
+        try:
+            found_email = data.get(search_string)['email']
+        except TypeError:
+            messagebox.showerror(title="Not Found", message="No password found.")
+            password_text.delete("1.0", END)
+            email_text.delete("1.0", END)
+            website_text.delete("1.0", END)
+        else:
+            found_password = data.get(search_string)['password']
+            messagebox.showinfo(title=f"Information for {search_string}",
+                                message=f"Email/Login: {found_email}\nPassword: {found_password}")
+            password_text.delete("1.0", END)
+            email_text.delete("1.0", END)
+            website_text.delete("1.0", END)
+
+
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -20,17 +54,31 @@ def add():
     website_string = website_text.get("1.0", END).strip()
     email_string = email_text.get("1.0", END).strip()
     password_string = password_text.get("1.0", END).strip()
+    new_data = {
+        website_string: {
+            "email":email_string,
+            "password":password_string,
+        }
+    }
     print(website_string)
     if website_string == "" or email_string == "" or password_string == "":
         messagebox.showerror(title="Missing", message="Incomplete Form")
     else:
-        is_ok = messagebox.askokcancel(title=website_string, message=f"These are the details: \n Website:"
-                                                                     f" {website_string}\n Email/Login: "
-                                                                     f"{email_string}\n Password: "
-                                                                     f"{password_string}\n Ok to save? ")
-        if is_ok:
-            with open("password.txt", mode="a") as file:
-                file.write(f"{website_string} | {email_string} | {password_string}\n")
+        try:
+            with open("data.json", mode="r") as data_file:
+                # reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+            with open("data.json", mode="w") as data_file:
+                # saving updated data to json file
+                json.dump(data, data_file, indent=4)
+        finally:
             messagebox.showinfo(title="Success", message="Data Saved")
             password_text.delete("1.0", END)
             email_text.delete("1.0", END)
@@ -62,7 +110,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=4, column=1)
 
 # Text
-website_text = Text(height=1, width=35, relief="solid", bd=1)
+website_text = Text(height=1, width=21, relief="solid", bd=1)
+
 # Puts cursor in textbox.
 # website_text.config()
 # website_text.focus()
@@ -70,9 +119,9 @@ website_text = Text(height=1, width=35, relief="solid", bd=1)
 website_text.insert(END, "")
 # Get's current value in textbox at line 1, character 0
 # print(text.get("1.0", END))
-website_text.grid(row=2, column=2, columnspan=2)
+website_text.grid(row=2, column=2)
 
-email_text = Text(height=1, width=35, relief="solid", bd=1)
+email_text = Text(height=1, width=36, relief="solid", bd=1)
 # Puts cursor in textbox.
 # email_text.focus()
 # Adds some text to begin with.
@@ -98,5 +147,9 @@ generate_button.grid(row=4, column=3)
 add_button = Button(text="Add", width=36, command=add)
 add_button.config()
 add_button.grid(row=5, column=2, columnspan=2)
+
+search_button = Button(text="Search",width=13, command=search)
+search_button.config()
+search_button.grid(row=2, column=3)
 
 window.mainloop()
